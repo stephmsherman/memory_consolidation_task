6bb8b65d13d66a5848e810516800534a70a32784
Stephanie Sherman <stephmsherman@gmail.com>
2017-02-22 08:47:29 -0500

    revise

--- words/randomize_words.R
+++ words/randomize_words.R
@@ -5,7 +5,7 @@ list = 1 # either 1 or 2
 #define path where you download memory_consolidation_task. Make sure to end in the /
 path="/"
 
-one=read.csv(paste("memory_consolidation_task/words/all_words_list",list,".csv",sep=""))
+one=read.csv(paste(path,"memory_consolidation_task/words/all_words_list",list,".csv",sep=""))
 
 head(one)
 
@@ -31,14 +31,14 @@ night_test= random_recall[1:((dim(one)[1])/2),]
 morning_test= random_recall[(((dim(one)[1])/2)+1):(dim(one)[1]),]
 
 #write out 
-write.csv(random_study,paste("memory_consolidation_task/study_list",list,"sub",participant,".csv",sep=""),row.names=FALSE,quote=FALSE)
+write.csv(random_study,paste(path,"memory_consolidation_task/study_list",list,"sub",participant,".csv",sep=""),row.names=FALSE,quote=FALSE)
 
-write.csv(random_study_recall,paste("memory_consolidation_task/study_recall_list",list,"sub",participant,".csv",sep=""),row.names=FALSE,quote=FALSE)
+write.csv(random_study_recall,paste(path,"memory_consolidation_task/study_recall_list",list,"sub",participant,".csv",sep=""),row.names=FALSE,quote=FALSE)
 
-write.csv(night_test,paste("memory_consolidation_task/night_recall_list",list,"sub",participant,".csv",sep=""),row.names=FALSE,quote=FALSE)
+write.csv(night_test,paste(path,"memory_consolidation_task/night_recall_list",list,"sub",participant,".csv",sep=""),row.names=FALSE,quote=FALSE)
 
 
-write.csv(morning_test,paste("memory_consolidation_task/morning_recall_list",list,"sub",participant,".csv",sep=""),row.names=FALSE,quote=FALSE)
+write.csv(morning_test,paste(path,"memory_consolidation_task/morning_recall_list",list,"sub",participant,".csv",sep=""),row.names=FALSE,quote=FALSE)
 
 
 
